e3c091abbdb6043311e5d5b382ff5611fabaa2f4
cebang97 <cebang@posicube.com>
2025-03-26 10:47:24 +0900

    practice v0.0.2

--- practice/tagtag.ts
+++ practice/tagtag.ts
@@ -1,1 +1,5 @@
 console.log("Hello, TypeScript!^^");
+console.log("Hello, TypeScript!^^");
+console.log("Hello, TypeScript!^^");
+console.log("Hello, TypeScript!^^");
+console.log("Hello, TypeScript!^^");
