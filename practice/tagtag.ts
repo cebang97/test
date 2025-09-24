87bead0ec3aeac9c55e3ff38326bd85da037fcb7
cebang97 <cebang@posicube.com>
2025-09-24 12:36:28 +0900

    testst

--- practice/tagtag.ts
+++ practice/tagtag.ts
@@ -1,6 +1,6 @@
 console.log("testing");
 console.log("Hello, TypeScript!^^");
 console.log("Hello, TypeScript!^^");
-console.log("Hello, TypeScript!^^");
+console.log("test222");
 console.log("test");
 console.log("Hello, TypeScript!^^");
